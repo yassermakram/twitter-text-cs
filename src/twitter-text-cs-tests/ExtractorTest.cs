5a2fa3f8e7a8c8d6b67526636cd38712eb52cff7
Yasser Makram <yasser@silverlightrcipes.com>
2011-05-07 21:12:04 +0200

    Extractor implementation for replies

---
+++ src/twitter-text-cs-tests/ExtractorTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TwitterText;
+
+namespace twitter_text_cs_tests
+{
+    public class ExtractorTest
+    {
+        private Extractor _extractor;
+        [SetUp]
+        public void Init()
+        {
+            _extractor = new Extractor();
+        }
+
+        [Test]
+        public void Reply_At_TheStart_Should_Return_User()
+        {
+            Assert.AreEqual("user", _extractor.ExtractReplyScreenname("@user reply"), "Failed to extract reply at the start");
+        }
+
+        [Test]
+        public void When_Extracting_Null_Should_Return_Null()
+        {
+            Assert.AreEqual(null, _extractor.ExtractReplyScreenname(null), "Failed to parse null tweet");
+        }
+    }
+}
---
+++ src/twitter-text-cs/Extractor.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TwitterText
+{
+    public class Extractor
+    {
+        public string ExtractReplyScreenname(string tweet)
+        {
+            if (string.IsNullOrEmpty(tweet))
+                return null;
+            var replyExpression = new Regex("^@([a-z0-9_]{1,20}).*", RegexOptions.IgnoreCase);
+
+            var match = replyExpression.Match(tweet);
+            if (match.Success)
+                return match.Groups[1].Value;
+            else
+                return null;
+        }
+    }
+}
