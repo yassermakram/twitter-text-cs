4d536fae1acb808c2a210eb39138ea67dc7d713d
Yasser Makram <yasser@silverlightrcipes.com>
2011-05-10 01:40:51 +0200

    AutoLinker first commit

---
+++ src/twitter-text-cs-tests/AutoLinkTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using TwitterText;
+
+namespace twitter_text_cs_tests
+{
+    [TestFixture]
+    public class AutoLinkTest
+    {
+        private AutoLink _linker;
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+    }
+}
